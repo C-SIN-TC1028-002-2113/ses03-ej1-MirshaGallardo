db5c968ccd8bc104d86d56955de8d79eda5ad8f2
Mirsha Gallardo <A01741024@ITESM.MX>
2021-08-17 14:38:08 +0000

    sesion3

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+    a = float(input("Area a pintar en metros: "))
+    r = float(input("Rendimiento (m2/l): "))
+    l = math.ceil(a/r)
+    print("Litros a comprar:",l)
+
+
+
     pass
 
 if __name__ == '__main__':
